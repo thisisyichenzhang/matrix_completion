3534b6232f582398ca52d64f0b0622db8f19a5d3
isaaczhangyc <zhangyichen93@gmail.com>
2018-04-08 16:06:50 -0700

    initial commit

---
+++ R_solver_change.R
@@ -0,0 +1,156 @@
+################ main solvers ##################
+# classic lasso solver  
+ggd_solver <- function(Y, mask, lambda, theta = NULL, num_it, thresh){
+  if(is.null(theta)){
+    theta <- Y * 0
+  }
+  norm_diff <- rep(0,num_it)
+  
+  for(it in 1:num_it){
+    U <- Y * mask + theta * (1-mask)
+    theta_new <- singular_soft_thresh(U,lambda)
+    norm_diff[it] <- mean((theta_new - theta)^2)
+    theta <- theta_new
+    if(norm_diff[it] < thresh) break;
+  }
+  return(list(theta=theta, norm_diff = norm_diff))
+}
+
+# squared root lasso solver 
+sqrt_ggd_solver <- function(Y, mask, lambda, theta = NULL, num_it, thresh, sigma = 1 ){
+  if(is.null(theta)){
+    theta <- Y * 0
+  }
+  norm_diff <- rep(0,num_it)
+  
+  for(it in 1:num_it){
+    U <- Y * mask + theta * (1-mask)
+    theta_new <- singular_soft_thresh(U,lambda * sigma)
+    norm_diff[it] <- mean((theta_new - theta)^2)
+    sigma_new = norm_obs(Y - theta_new * mask)
+    sigma = sigma_new
+    theta = theta_new
+    if(norm_diff[it] < thresh) break;
+  }
+  return(list(theta=theta, norm_diff = norm_diff,sigma = sigma))
+}
+################ end of main solvers ##################
+
+############## little functions ######################
+# singular soft threshold for the matrice 
+singular_soft_thresh <- function(U, lambda){
+  junk <- svd(U)
+  new_singular_vals <- pmax(junk$d - lambda, 0)
+  new_U <- junk$u %*% diag(new_singular_vals) %*% t(junk$v)
+  return(new_U)
+}
+
+# Mask function 
+## this is for later use, Here the mask is generated with the data, so no need.   
+## mask function - indicator 0/1 matrix of observed/unobseved entries 
+id_obs = function(x){
+  vec_x = as.vector(x)
+  vec_x[which(vec_x != 0)] <- 1
+  
+  return(matrix(vec_x,nrow = nrow(x), ncol=ncol(x)))
+}
+
+# generating random rank-specified sparse matrix
+gen_data <- function(n, rank, percent_missing, sigma){
+  mu_init <- matrix(rnorm(n*n,mean = 3), ncol = n)
+  junk <- svd(mu_init)
+  if(rank != 1) mu = junk$u[,1:rank] %*% diag(junk$d[1:rank]) %*% t(junk$v[,1:rank])
+  if(rank == 1 ) mu = junk$u[,1] %*% t(junk$v[,1]) * junk$d[1] 
+  Y_init <- mu + rnorm(n*n,sd = sigma)
+  mask <- matrix(rbinom(n*n, 1, 1-percent_missing), ncol = n)
+  Y_obs <- Y_init * mask
+  
+  return(list(mask = mask, Y = Y_obs, mu = mu, SNR = norm(mu, type = "F")/(n*sigma)))
+  #that is the same as calculating SNR <- norm(gen$mu, type = "F")/ norm(noise, type = "F")
+}
+
+# Frobenius norm for the non-zeros (observed) entries 
+norm_obs<-function(x){
+  n_obs <- length(which(x!=0))
+  n <- nrow(x) * ncol(x)
+  return(sqrt(norm(x,type = "F")^2 * n / n_obs ))
+}
+
+#In my implementation I wrote two little helper functions (to determine the foldids and the lambda_sequence
+#if none are specified)
+create.lambda <- function(data){
+  return(seq(0,max(svd(data)$d),by = 1))
+}
+create.folds <- function(data, mask, nfold){
+  length <- nrow(data)^2
+  fold_vector <- sample(1:nfold, length, replace = TRUE)
+  fold_matrix <- matrix(fold_vector, nrow = nrow(data)) * mask
+  return(fold_matrix)
+}
+############## end of little functions ######################
+
+############ cross validation ##############################
+# cross valadation for classic lasso 
+cv_matrixcmplt_ggd <- function(data,mask,true,theta = NULL, foldid = NULL, lambda_sequence = NULL, nfold = 10, thresh = 0.01){
+  if(is.null(lambda_sequence)) lambda_sequence <- create.lambda(data)
+  if(is.null(foldid))          foldid <- create.folds(data, mask, nfold)
+  
+  error.by.fold <- numeric(nfold)
+  error.by.lambda <- numeric(length(lambda_sequence))
+  
+  # warm start for the fold 1
+  for(nlambda in 1:length(lambda_sequence)){
+    mask_tst = foldid
+    mask_tst[ (mask_tst == 1) ] = 0
+    mask_tst[ (mask_tst != 0) ] = 1
+    results <- ggd_solver(Y = data * mask_tst,theta = theta, mask = mask_tst,lambda = lambda_sequence[nlambda],num_it = 1000,thresh = thresh )
+    tst <- results$theta  
+    error.by.fold[1] <- norm(tst * (1-mask_tst) - true * (1-mask_tst), type = "F") 
+    ## using the initial beta as the warm start (for the lefting folds other than fold 1 )
+    for(fold in 2:nfold){
+      mask_tst = foldid
+      mask_tst[ (mask_tst == fold) ] = 0
+      mask_tst[ (mask_tst != 0) ] = 1
+      results <- ggd_solver(Y = data * mask_tst,mask = mask_tst, theta = tst,lambda = lambda_sequence[nlambda],num_it = 1000,thresh = thresh )
+      tst <- results$theta 
+      error.by.fold[fold] <- norm(tst * (1-mask_tst) - true * (1-mask_tst),type = "F")
+    }
+    error.by.lambda[nlambda] <- mean(error.by.fold) 
+  }
+  return(list(lambdas = lambda_sequence, errors = error.by.lambda ))
+} 
+
+
+# cross validation for square-root lasso 
+cv_matrixcmplt_ggd_sqr <- function(data, mask,true, theta = NULL, foldid = NULL, lambda_sequence = NULL, nfold = 10, thresh = 0.01){
+  if(is.null(lambda_sequence)) lambda_sequence <- create.lambda(data)
+  if(is.null(foldid))          foldid <- create.folds(data, mask, nfold)
+  
+  error.by.fold <- numeric(nfold)
+  error.by.lambda <- numeric(length(lambda_sequence))
+  
+  # warm start for the fold 1
+  for(nlambda in 1:length(lambda_sequence)){
+    mask_tst = foldid
+    mask_tst[ (mask_tst == 1) ] = 0
+    mask_tst[ (mask_tst != 0) ] = 1
+    results <- sqrt_ggd_solver(Y = data * mask_tst,theta = theta, mask = mask_tst,lambda = lambda_sequence[nlambda],num_it = 1000,thresh = thresh )
+    tst <- results$theta  
+    error.by.fold[1] <- norm(tst * (1-mask_tst) - true * (1-mask_tst),type = "F")
+    
+    ## using the initial beta as the warm start (for the lefting folds other than fold 1 )
+    for(fold in 2:nfold){
+      mask_tst = foldid
+      mask_tst[ (mask_tst == fold) ] = 0
+      mask_tst[ (mask_tst != 0) ] = 1
+      results <- sqrt_ggd_solver(Y = data * mask_tst, mask = mask_tst, theta = tst,lambda = lambda_sequence[nlambda],num_it = 1000,thresh = thresh )
+      tst <- results$theta 
+      error.by.fold[fold] <- norm(tst * (1-mask_tst) - true * (1-mask_tst),type = "F")
+    }
+    error.by.lambda[nlambda] <- mean(error.by.fold) 
+  }
+  return(list(lambdas = lambda_sequence, errors = error.by.lambda ))
+} 
+
+############ end of cross validation ###########################
+
---
+++ sim_ggd.R
@@ -0,0 +1,70 @@
+source("./R_solver_change.R")
+
+#gen_0<-gen_data(n = 50,rank = 2,percent_missing = 0.01,sigma = 0) # SNR INF
+#gen_0.01<-gen_data(n = 50,rank = 2,percent_missing = 0.01,sigma = 0.01) # SNR 300
+#gen_0.1<-gen_data(n = 50,rank = 2,percent_missing = 0.01,sigma = 0.1) # SNR 30
+#gen_0.5<-gen_data(n = 50,rank = 2,percent_missing = 0.01,sigma = 0.5) # SNR 6
+#gen_1<-gen_data(n = 50,rank = 2,percent_missing = 0.1,sigma = 2) # SNR 3.0
+#gen_10<-gen_data(n = 50,rank = 2,percent_missing = 0.01,sigma = 10) # SNR 0.3
+#gen_100<-gen_data(n = 50,rank = 2,percent_missing = 0.01,sigma = 100) # SNR 0.03
+
+# find the optimal lambda
+#results$lambdas[which.min(results$errors)]
+
+# data frame for save
+# if existing use the following command instead 
+# load("./Results/sqr_results.Rdata")
+sim_results_ggd<-data.frame(n=numeric(),
+                        rank=integer(),
+                        percent_missing=numeric(),
+                        sigma=numeric(),
+                        SNR = numeric(),
+                        lambda=numeric(),
+                        error=numeric())
+# seed for reproducibility
+set.seed(666)
+# number of simulations per set 
+k=1000
+# results for each set of training values
+results<-numeric(k)
+# training values
+n_vec<-c(50,100)
+rank_vec<-c(2,3,4,5)
+percent_missing_vec<-c(0.01,0.05,0.1,0.2,0.3,0.4,0.5)
+sigma_vec<-c((0:9)*0.01,0.1,0.5,1,10)
+lambda_vec<-c(seq(0,1,0.1))
+
+for(n in n_vec){
+  for(rank in rank_vec){
+    for(percent_missing in percent_missing_vec){
+      for(sigma in sigma_vec){
+        sim_dat<-gen_data(n = n,rank = rank,percent_missing = percent_missing, sigma = sigma) 
+        for(lambda in lambda_vec){
+          for(i in 1:k){
+            results[i]<-cv_matrixcmplt_ggd(theta = sim_dat$mu,
+                                               lambda_sequence = lambda,
+                                               data = sim_dat$Y,
+                                               mask = sim_dat$mask,
+                                               true = sim_dat$mu,
+                                               thresh = 1e-6,
+                                               nfold = 10)$error
+          }
+          sim_results_ggd<-rbind(sim_results_ggd,
+                             data.frame(n=n,
+                                        rank=rank,
+                                        percent_missing=percent_missing,
+                                        sigma=sigma,
+                                        SNR=sim_dat$SNR,
+                                        lambda=lambda,
+                                        error=mean(results)))
+          save(sim_results_ggd,file = "./Results/ggd_results.Rdata")
+        }
+      }
+    }
+  }
+}
+
+
+#library(data.table)
+#sim_results_ggd<-as.data.table(sim_results_ggd)
+#sim_results_ggd[,.(optimal.lambda=lambda[which.min(error)]),by=SNR]
